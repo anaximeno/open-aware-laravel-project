ac133fee2213ef8db02061b9d4eef1f091ba1f34
anaximeno <anaximenobrito@gmail.com>
2022-08-22 23:51:37 -0100

    feat: add project seeder

--- my-project/database/seeders/DatabaseSeeder.php
+++ my-project/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->has(Project::factory()
+            ->count(3)
+            ->state(function (array $attributes, User $user) {
+                return ['creator_id' => $user->id];
+            }), 'projectsCreated')->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
